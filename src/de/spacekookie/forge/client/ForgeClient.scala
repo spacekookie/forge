7dbe688b51f350c96e59a35aa08e2f56df42d76d
Katharina Sabel <sabel.katharina@gmail.com>
2017-02-18 18:58:27 +0100

    Adding config parsing library and first code

--- src/de/spacekookie/forge/client/ForgeClient.scala
+++ src/de/spacekookie/forge/client/ForgeClient.scala
@@ -1,8 +1,20 @@
 package de.spacekookie.forge.client
 
+import com.typesafe.config._
+
 object ForgeClient {
+  val home: String = System.getProperty("user.home")
+  val cache: String = home + "/.local/share/forge/"
+  val config: String = home + "/.config/forge/"
   
-  def main(args: Array[String]) {
+  def main(args: Array[String]): Unit = {
+    println("Starting forge client...")
+    println(s"Config directory '$config'")
+    println(s"Cache directory '$cache'")
+    
+    /** Load the client configuration from config directory */
+    val clientConf = config + "client.conf"
+    var conf = ConfigFactory.load();
     
   }
 }
