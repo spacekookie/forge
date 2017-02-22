504c4892e8a4db6e59fa5f36a956fa5ec0320b89
Katharina Sabel <sabel.katharina@gmail.com>
2017-02-22 17:01:48 +0100

    Configuration files can now be created and sorta read

--- src/de/spacekookie/forge/client/Configs.scala
+++ src/de/spacekookie/forge/client/Configs.scala
@@ -4,31 +4,39 @@ import play.api.libs.json._
 
 /** Define base models for data storage from json objects  */
 case class Server(url: String, port: Int, authToken: Option[String])
-case class ClientConf(host: String, server: Server, gitKey: String, twoWay: Boolean)
+case class ClientConf(host: String, server: Option[Server], key: Option[String], twoWay: Boolean)
 
 object Server {
   implicit val serverFormats = Json.format[Server]
 
-  def write(server: Server) = Json.toJson(server)
-  def read(jsonServer: JsValue): Server = jsonServer.as[Server]
+  def write(server: Option[Server]) = Json.toJson(server)
+  def read(json: JsValue): Option[Server] = Option(json.as[Server])
 }
 
 object ClientConf {
+  implicit val clientFormats = Json.format[ClientConf]
 
   def write(conf: ClientConf) = {
+
     JsObject(Seq(
       "hostname" -> JsString(conf.host),
+      "two_way" -> JsBoolean(conf.twoWay),
       "server" -> Json.toJson(conf.server),
-      "git_key" -> JsString(conf.gitKey),
-      "two_way" -> JsBoolean(conf.twoWay)))
+
+      /** Gracefully handle non existant key */
+      "git_key" -> JsString(
+        conf.key match {
+          case None => ""
+          case _ => conf.key.get
+        }) /* end */ ))
   }
 
   def read(json: JsValue): ClientConf = {
     val host = (json \ "hostname").as[String]
     val server = (json \ "server").as[Server]
-    val gitKey = (json \ "git_key").as[String]
+    val key = (json \ "git_key").as[String]
     val twoWay = (json \ "two_way").as[Boolean]
 
-    return ClientConf(host, server, gitKey, twoWay)
+    return ClientConf(host, Option(server), Option(key), twoWay)
   }
 }
--- src/de/spacekookie/forge/client/ForgeClient.scala
+++ src/de/spacekookie/forge/client/ForgeClient.scala
@@ -1,20 +1,76 @@
 package de.spacekookie.forge.client
 
-import com.typesafe.config._
+import java.nio.file.{ Paths, Files }
+import play.api.libs.json.{ JsValue, Json }
+
+import scala.io.Source
+import java.io.{ File, FileWriter, PrintWriter }
 
 object ForgeClient {
   val home: String = System.getProperty("user.home")
   val cache: String = home + "/.local/share/forge/"
   val config: String = home + "/.config/forge/"
-  
+
+  /** Place to store future configuration safel */
+  var clientConf: Option[ClientConf] = null
+
   def main(args: Array[String]): Unit = {
     println("Starting forge client...")
     println(s"Config directory '$config'")
     println(s"Cache directory '$cache'")
-    
-    /** Load the client configuration from config directory */
+
+    /** Build the config path and check for existance */
     val clientConf = config + "client.conf"
-    var conf = ConfigFactory.load();
-    
+    val exists = Files.exists(Paths.get(clientConf))
+
+    /** Either load config or create new one */
+    exists match {
+      case true => checkConfig(clientConf)
+      case false => createConfig(clientConf)
+    }
   }
+
+  private def checkConfig(path: String) {
+    println("Loading existing config")
+
+    /** Read the config file carefully */
+    val source = Source.fromFile(path)
+    val lines: String = try source.mkString finally source.close()
+    val json: JsValue = Json.parse(lines)
+
+    /** Then create a new ClientConf from data */
+    val cc: ClientConf = ClientConf.read(json)
+    clientConf = Option(cc)
+  }
+
+  private def createConfig(path: String) {
+    println("Creating new config")
+
+    /** First make sure our directory exists */
+    new File(path).createNewFile()
+    
+    /** Then load data about system */
+    val source = Source.fromFile("/etc/hostname")
+    
+    /** Hostname and twoway sync are always present  */
+    val host: String = try source.mkString.replace("\n", "") finally source.close()
+    val twoWay: Boolean = false
+
+    /** Create a new client config with some empty fields */
+    val cc: ClientConf = ClientConf(host, None, None, twoWay)
+    clientConf = Option(cc)
+
+    /** Encode config to json and write to disk */
+    updateConfig(path, cc)
+  }
+
+  /** Write a client conf to disk for future use */
+  private def updateConfig(path: String, conf: ClientConf) = {
+    val json: String = ClientConf.write(conf).toString();
+    val writer = new PrintWriter(new File(path))
+    writer.write(json)
+    writer.close()
+    println("Writing config complete")
+  }
+
 }
